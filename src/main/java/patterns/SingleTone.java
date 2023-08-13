5a371ea1a736b82e28324626ef938721291cf4f3
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2023-08-13 13:08:15 +0400

    Simple Singletone

--- src/main/java/patterns/SingleTone.java
+++ src/main/java/patterns/SingleTone.java
@@ -1,11 +1,18 @@
 package patterns;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class SingleTone {
 
-  WebDriver driver;
-  SingleTone singleTone;
+  private static WebDriver driver;
+  private static SingleTone singleTone;
+
+  private SingleTone() {
+    System.setProperty("webdriver.chrome.driver", "C:\\WebDriver\\chromedriver.exe");
+    // Initialization of WebDriver for the Chrome browser
+    driver = new ChromeDriver();
+  }
 
   public WebDriver getDriver() {
     return driver;
