b0a52b011c0a192cf177dd7db0a7419cac0a61d0
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2025-01-27 20:14:18 +0000

    Constructors in Inheritance

--- src/main/java/java_course_tau/chapter8_data_types/PasswordValidator.java
+++ src/main/java/java_course_tau/chapter8_data_types/PasswordValidator.java
@@ -2,6 +2,10 @@ package java_course_tau.chapter8_data_types;
 
 public class PasswordValidator {
 
+//    You'll need to verify the strength of a proposed password change.
+//    The password must be at least eight characters long, contain an uppercase letter,
+//    contain a special character, not contain the username and not be the same as the old password.
+
     public boolean validatePassword(String newPassword, String username, String oldPassword) {
         // Проверка на минимальную длину
         if (newPassword.length() < 8) {
---
+++ src/main/java/java_course_tau/chapter9_inheritance/InhiretanceTester.java
@@ -0,0 +1,10 @@
+package java_course_tau.chapter9_inheritance;
+
+public class InhiretanceTester {
+
+    public static void main(String[] args) {
+
+//        Person person = new Person();
+        Employee employee = new Employee();
+    }
+}
