bf556a272144afb39b643379af30f11fbbed4fe2
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2025-02-02 19:22:21 +0000

    Overriding and Overloading in Inheritance

--- src/main/java/java_course_tau/chapter9_inheritance/InhiretanceTester.java
+++ src/main/java/java_course_tau/chapter9_inheritance/InhiretanceTester.java
@@ -4,7 +4,25 @@ public class InhiretanceTester {
 
     public static void main(String[] args) {
 
-//        Person person = new Person();
-        Employee employee = new Employee();
+//      Person person = new Person();
+//      Employee employee = new Employee();
+
+        Rectangle rectangle = new Rectangle();
+        rectangle.print();
+
+        Square square = new Square();
+        square.print("square");
+    }
+
+    public static void testSquareOverride(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(4);
+        rectangle.setWidth(8);
+        System.out.println(rectangle.calculatePerimeter());
+
+        Square square = new Square();
+        square.setLength(4);
+        square.setWidth(8);
+        System.out.println(square.calculatePerimeter());
     }
 }
