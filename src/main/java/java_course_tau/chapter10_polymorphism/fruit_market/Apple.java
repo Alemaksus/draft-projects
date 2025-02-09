3292b0238463f98fdb9dce43650e1f584aebbc56
Aleksandr Maksakov <58271708+Alemaksus@users.noreply.github.com>
2025-02-09 18:05:39 +0000

    Polymorphism with examples in Zoo and Fruit Market packages

---
+++ src/main/java/java_course_tau/chapter10_polymorphism/fruit_market/Apple.java
@@ -0,0 +1,17 @@
+package java_course_tau.chapter10_polymorphism.fruit_market;
+
+public class Apple extends Fruit {
+
+    public Apple() {
+        super(95); // Setting calories for an apple
+    }
+
+    public void removeSeeds() {
+        System.out.println("Seeds removed from the apple.");
+    }
+
+    @Override
+    public void makeJuice() {
+        System.out.println("Apple juice is made.");
+    }
+}
